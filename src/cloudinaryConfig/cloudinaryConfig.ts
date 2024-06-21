6423d6dfdfa6836fbf8b85fd887ff7e126918dda
Ravindra <sirviravindra609@gmail.com>
2024-06-21 09:38:18 +0530

    chore: Add .env to .gitignore and configure Cloudinary integration

---
+++ src/cloudinaryConfig/cloudinaryConfig.ts
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from "cloudinary";
+import { UploadApiErrorResponse } from "cloudinary";
+import { UploadApiResponse } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export { cloudinary };
+
+type UploadResponse =
+  | { success: true; result?: UploadApiResponse }
+  | { success: false; error: UploadApiErrorResponse };
+
+export const uploadToCloudinary = (
+  fileUri: string,
+  fileName: string
+): Promise<UploadResponse> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload(fileUri, {
+        invalidate: true,
+        resource_type: "auto",
+        filename_override: fileName,
+        folder: "product-images",
+        use_filename: true,
+      })
+      .then((result) => {
+        resolve({ success: true, result });
+      })
+      .catch((error) => {
+        reject({ success: false, error });
+      });
+  });
+};
