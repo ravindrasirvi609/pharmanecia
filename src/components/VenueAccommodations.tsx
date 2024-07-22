b947024551df24a0331f59a7609bcf18e4535115
Ravindra <sirviravindra609@gmail.com>
2024-07-22 16:44:47 +0530

    refactor: Update VenueAccommodations component to use updated Google Maps embed URL

--- src/components/VenueAccommodations.tsx
+++ src/components/VenueAccommodations.tsx
@@ -22,7 +22,7 @@ const VenueAccommodations = () => {
           </p>
           <div className="flex justify-center mb-4">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3915.490983208213!2d76.69538641531514!3d11.411236891851387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba859a9468ea909%3A0x2d67c252db4ea5df!2sJSS%20College%20of%20Pharmacy!5e0!3m2!1sen!2sin!4v1623278749595!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3911.0883078143543!2d76.70452457581243!3d11.401132147358766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba8bd9f179dd8ed%3A0xe380d17adde20a10!2sJSS%20College%20Of%20Pharmacy%20Ooty!5e0!3m2!1sen!2sin!4v1721646776209!5m2!1sen!2sin"
               width="600"
               height="450"
               allowFullScreen={true}
