614ef1e0e98743b06f84bf57a90d8617946603a0
Ravindra <sirviravindra609@gmail.com>
2024-11-17 21:19:15 +0530

    Update Early Bird offer validity in Registration page

--- src/app/Registration/page.tsx
+++ src/app/Registration/page.tsx
@@ -54,13 +54,13 @@ const Registration = () => {
             </Link>
           </p>
           <p className="text-lg leading-relaxed text-justify mb-4 font-bold">
-            Early Bird offer is valid until November 15, 2024.{" "}
+            Early Bird offer is valid until November 30, 2024.{" "}
           </p>
           <p className="text-lg leading-relaxed text-justify mb-4">
             All prices mentioned are inclusive of GST.{" "}
           </p>
         </div>
-        <InfiniteRibbon text="Early Bird offer is valid until November 15, 2024." />
+        <InfiniteRibbon text="Early Bird offer is valid until November 30, 2024." />
         {/* Registration Plans */}
         <RegistrationPlans />
       </div>
