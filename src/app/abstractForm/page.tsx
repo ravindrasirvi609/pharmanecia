4c16bc6ddaaf89b06ae2d7caff76523fe75aabc8
Ravindra <sirviravindra609@gmail.com>
2024-11-27 12:06:31 +0530

    Update framer-motion to version 11.11.17

--- src/app/abstractForm/page.tsx
+++ src/app/abstractForm/page.tsx
@@ -106,7 +106,7 @@ const AbstractFormPage: React.FC = () => {
                     into the designated space on the submission portal.
                   </li>
                   <li>Abstracts must be submitted online only.</li>
-                  <li>The deadline for submission is 15 November, 2024.</li>
+                  <li>The deadline for submission is 30 November, 2024.</li>
                   <li>
                     A model abstract is available for reference on the website.
                   </li>
